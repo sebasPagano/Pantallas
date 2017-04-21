b3c52d969dd533556bc15d6ae5a1bd9baa94d7bb
sebasPagano <sebas_el14@live.com.ar>
2017-04-20 21:57:16 -0300

    Menu

--- app/src/main/java/com/example/alumno/pantallas/MainActivity.java
+++ app/src/main/java/com/example/alumno/pantallas/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.alumno.pantallas;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nuevo_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Log.d("click","opcion del menu");
+        return super.onOptionsItemSelected(item);
+
     }
 }
